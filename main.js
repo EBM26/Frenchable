37b9c50b6632fa365a0c933083cdbcabace99b1e
Eyal Benmoshe <ebm2626@yahoo.com>
2015-08-28 11:19:58 -0700

    setting up ui-router with no success

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 angular
-    .module("frenchableApp", []);
+    .module("frenchableApp", ['ngResource','ui.router']);
+
+angular
+    .module("frenchableApp")
+    .config(function($stateProvider, $urlRouterProvider) {
+
+      // For any unmatched url, redirect to /home
+
+        $urlRouterProvider.otherwise('/home');
+
+        // setting up the states (routes)
+
+        $stateProvider
+
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+
+        .state('about', {
+            url: '/about',
+            templateUrl: 'partial-about.html'
+        })
+
+      });
+  
 
 
