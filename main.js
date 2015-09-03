47794c7ea703acb521ffb35bf9b258ee7701155a
Eyal Benmoshe <ebm2626@yahoo.com>
2015-09-03 12:51:32 -0700

    changed sample array and made ng-repeat

--- main.js
+++ main.js
@@ -1,52 +1,54 @@
 angular
-    .module("frenchableApp", ['ui.router']);
+.module("frenchableApp", ['ui.router']);
 
 angular
-    .module("frenchableApp")
-    .config(function($stateProvider, $urlRouterProvider) {
+.module("frenchableApp")
+.config(function($stateProvider, $urlRouterProvider) {
 
       // For any unmatched url, redirect to /home
 
-        $urlRouterProvider.otherwise('/home');
+      $urlRouterProvider.otherwise('/home');
 
         // setting up the states (routes)
 
         $stateProvider
 
         .state('home', {
-            url: '/home',
-            templateUrl: 'partial-home.html'
+          url: '/home',
+          templateUrl: 'partial-home.html'
         })
 
         .state('about', {
-            url: '/about',
-            templateUrl: 'partial-about.html'
+          url: '/about',
+          templateUrl: 'partial-about.html'
         })
 
         .state('products', {
-            url: '/products',
-            templateUrl: 'partial-products.html'
+          url: '/products',
+          templateUrl: 'partial-products.html'
         })
 
         .state('cart', {
-            url: '/cart',
-            templateUrl: 'partial-cart.html'
+          url: '/cart',
+          templateUrl: 'partial-cart.html'
         })
 
       });
-  
 
-  angular
-    .module("frenchableApp")
-    .controller("frechableController", function ($scope) {
 
-    $scope.pictures = [
-     {"name": "Red Leather Bag", "image": "images/FrenchHandbag.jpg", "price": "$99.99"},
-     {"name": "Winter Gloves", "image": "images/FrenchGloves.jpg", "price": "$39.99"},
-     {"name": "French Red Hat", "image": "images/FrenchHat.jpeg", "price": "$69.99"}
+angular
+.module("frenchableApp")
+.controller("frechableController", function ($scope) {
 
-    ]
-  });
+//samples array for home page
+
+  $scope.samples = [
+  {"name": "Stylish Bags", "image": "images/FrenchHandbag.jpg"},
+  {"name": "Stylish Gloves", "image": "images/FrenchGloves.jpg"},
+  {"name": "Stylish Hats", "image": "images/FrenchHat.jpeg"}
+
+  ]
+});
 
 
 
