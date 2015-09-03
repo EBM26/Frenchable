7bd658fcdfca8d74b62824c6d4f64793b531ed0b
Eyal Benmoshe <ebm2626@yahoo.com>
2015-09-03 14:20:37 -0700

    set up products ng-repeat

--- main.js
+++ main.js
@@ -48,6 +48,34 @@ angular
   {"name": "Stylish Hats", "image": "images/FrenchHat.jpeg"}
 
   ]
+
+  // hats array for products page
+
+  $scope.hats = [
+  {"name": "Red French Hat", "image": "images/Hat1.jpg", "price": "$39.99"},
+  {"name": "White French Hat", "image": "images/Hat4.jpg", "price": "$29.99"},
+  {"name": "Green French Hat", "image": "images/Hat3.jpg", "price": "$49.99"}
+
+  ]
+
+  //  gloves array for products page
+
+  $scope.gloves = [
+  {"name": "Black Leather Gloves", "image": "images/Gloves1.jpg", "price": "$59.99"},
+  {"name": "French Pattern Gloves", "image": "images/Gloves2.jpg", "price": "$49.99"},
+  {"name": "French Mesh Gloves", "image": "images/Gloves3.jpg", "price": "$69.99"}
+
+  ]
+
+
+  //  bags array for products page
+   $scope.bags = [
+  {"name": "Pink Leather Bag", "image": "images/Bag1.jpg", "price": "$79.99"},
+  {"name": "French Pattern Bag", "image": "images/Bag2.jpg", "price": "$69.99"},
+  {"name": "Purple Flower Bag", "image": "images/Bag3.jpg", "price": "$89.99"}
+  
+  ]
+
 });
 
 
